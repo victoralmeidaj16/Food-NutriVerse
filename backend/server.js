735f7490a35ec6b0cf722379db4b6985d0fa7b8f
victoralmeidaj16 <victor.almeida.jeremias@gmail.com>
2025-12-11 19:10:52 -0300

    Aumentar limite de upload para 50mb

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 const PORT = process.env.PORT || 3000;
 
