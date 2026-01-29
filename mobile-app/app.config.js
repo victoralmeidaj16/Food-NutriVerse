60c5421179567b74fd9d161702bc3ffe052c116e
victoralmeidaj16 <victor.almeida.jeremias@gmail.com>
2026-01-29 14:25:07 -0300

    chore: bump version to 1.0.0 (13)

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -17,7 +17,7 @@ module.exports = {
         ios: {
             supportsTablet: true,
             bundleIdentifier: "com.victoralmeidaj16.foodnutriverse",
-            buildNumber: "12",
+            buildNumber: "13",
             infoPlist: {
                 NSCameraUsageDescription: "Precisamos de acesso à câmera para você escanear ingredientes e gerar receitas personalizadas.",
                 NSPhotoLibraryUsageDescription: "Precisamos de acesso à galeria para você escolher fotos de ingredientes para análise.",
@@ -30,7 +30,7 @@ module.exports = {
                 backgroundColor: "#ffffff"
             },
             package: "com.victoralmeidaj16.foodnutriverse",
-            versionCode: 12,
+            versionCode: 13,
             edgeToEdgeEnabled: true,
             predictiveBackGestureEnabled: false
         },
